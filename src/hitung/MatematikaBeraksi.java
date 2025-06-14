f5c70711277154b0c27b7901599dc4960895e02e
Mrl4n0r <lanamau368@gmail.com>
2025-06-14 11:52:13 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author Lenovo LOQ
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika lana = new Matematika(2, 3);
+
+        System.out.println("Hasil penjumlahan: " + lana.setPenjumlahan());
+        System.out.println("Hasil pengurangan: " + lana.setPengurangan());
+        System.out.println("Hasil perkalian: " + lana.setPerkalian());
+        System.out.println("Hasil pembagian: " + lana.setPembagian());
+    }
 }
